bbb931b74deacc0cf47d8ea974efb070660ad3cb
Elève EKOD 01 <eleve.ekod1@cciformation72.fr>
2025-09-10 12:28:24 +0200

    feat: added get completed tasks

--- server.js
+++ server.js
@@ -5,35 +5,38 @@ const port = 4000;
 
 app.use(express.json());
 
-let tasks = [{id: 0, title: "test", status: false} ,{id: 1, title: "lorem ipsum", status: false}];
+let tasks = [{ id: 0, title: "test", status: false }, { id: 1, title: "lorem ipsum", status: false }];
 let id = 0;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('hello');
 });
 
-app.get('/tasks', (req, res) =>{
-    res.json(tasks);
+app.get('/tasks', (req, res) => {
+    if (req.query.status == "completed") {
+        res.json(tasks.filter(task => task.status == true));
+    }
+    else { res.json(tasks); }
 });
 
-app.post('/tasks', (req, res) =>{
+app.post('/tasks', (req, res) => {
     const { title, status } = req.body;
     const newTask = {
         id: tasks.length > 0 ?
-        tasks[tasks.length - 1].id + 1 : 1,
+            tasks[tasks.length - 1].id + 1 : 1,
         title,
         status: status || false,
     };
-        tasks.push(newTask);
-        res.status(200).json(newTask);
+    tasks.push(newTask);
+    res.status(200).json(newTask);
 })
 
-app.put('/tasks/:id', (req, res) =>{
+app.put('/tasks/:id', (req, res) => {
     const taskId = parseInt(req.params.id);
     const task = tasks.find((t) => t.id === taskId);
 
-    if(!task) {
-        res.status(404).json({message: "Task not found"});
+    if (!task) {
+        res.status(404).json({ message: "Task not found" });
         return
     }
 
@@ -44,21 +47,21 @@ app.put('/tasks/:id', (req, res) =>{
     res.json(task);
 });
 
-app.delete('/tasks/:id', (req, res) =>{
+app.delete('/tasks/:id', (req, res) => {
     const taskId = parseInt(req.params.id);
     const index = tasks.findIndex((t) => t.id === taskId);
 
-    if( index === -1) {
-        res.status(404).json({message: "Task not found"});
+    if (index === -1) {
+        res.status(404).json({ message: "Task not found" });
         return
     }
 
     const deletedTask = tasks.splice(index, 1);
-    res.status(200).json(deletedTask, {message: "Task succesfully deleted"});
+    res.status(200).json(deletedTask, { message: "Task succesfully deleted" });
 
 
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`The server is online and ready at http://localhost:${port}`);
 });
